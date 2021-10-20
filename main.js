70a71f80c4938c2561efb218c21f34af0d362760
2440986F <2440986F@student.gla.ac.uk>
2021-10-20 13:58:08 +0100

    added movement controls

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import './style.css'
+import './style.css';
 
 import * as THREE from 'three';
 
@@ -23,17 +23,40 @@ camera.setRotationFromAxisAngle(new THREE.Vector3( 0, 1, 0 ), Math.PI/4)
 const geometry = new THREE.BoxGeometry(10,10,10);
 const material = new THREE.MeshPhongMaterial({color: 0xFF0066});
 const cube1 = new THREE.Mesh(geometry, material);
+cube1.position.set(0,5,0)
 
 scene.add(cube1);
 
-const light = new THREE.HemisphereLight(0xB1E1FF, 0xB97A20, 0.75);
+const light = new THREE.DirectionalLight(0xFFFFFF, 1);
+light.position.set(100, 100, 200);
+light.target.position.set(0, 0, 0);
 scene.add(light);
+scene.add(light.target);
 
-var goingUp = true;
+
+const planeSize = 400;
+     
+const loader = new THREE.TextureLoader();
+const texture = loader.load('resources/images/checker.png');
+texture.wrapS = THREE.RepeatWrapping;
+texture.wrapT = THREE.RepeatWrapping;
+texture.magFilter = THREE.NearestFilter;
+const repeats = planeSize / 2;
+texture.repeat.set(repeats, repeats);
+
+const planeGeo = new THREE.PlaneGeometry(planeSize, planeSize);
+const planeMat = new THREE.MeshPhongMaterial({
+  map: texture,
+  side: THREE.DoubleSide,
+});
+const mesh = new THREE.Mesh(planeGeo, planeMat);
+mesh.rotation.x = Math.PI * -.5;
+scene.add(mesh);
+
 
 function animate(){
   requestAnimationFrame(animate);
-
+/*
   if (cube1.position.y < 20 && goingUp){
     cube1.position.y += 0.2;
     cube1.position.z += 0.1;
@@ -48,9 +71,11 @@ function animate(){
 
   cube1.rotation.y += 0.01;
   cube1.rotation.x += 0.01;
-
+*/
   document.getElementById("coords").innerHTML = "[" + cube1.position.x.toFixed(2) + ", " + cube1.position.y.toFixed(2) + ", " + cube1.position.z.toFixed(2) + "]";
-  document.getElementById("rotation").innerHTML = cube1.rotation.y.toFixed(2);
+  document.getElementById("rotation-x").innerHTML = cube1.rotation.x.toFixed(2);
+  document.getElementById("rotation-y").innerHTML = cube1.rotation.y.toFixed(2);
+  document.getElementById("rotation-z").innerHTML = cube1.rotation.z.toFixed(2);
   renderer.render(scene, camera);
 }
 
@@ -58,7 +83,11 @@ function newColour(){
   cube1.material.color.set(Math.floor(Math.random()*16777215));
 }
 
-document.getElementById("colourButton").onclick = newColour;
 
+document.getElementById("colourButton").onclick = newColour;
+document.getElementById("upButton").onclick = function(){cube1.position.y += 0.4;};
+document.getElementById("downButton").onclick = function(){cube1.position.y -= 0.4;};
+document.getElementById("leftButton").onclick = function(){cube1.position.x -= 0.4;};
+document.getElementById("rightButton").onclick = function(){cube1.position.x += 0.4;};
 
 animate();
