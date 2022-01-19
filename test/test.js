c7b15b0810f2198345cf08ebb084b2d14abac664
Jake Lawrence <jakesamuellawrence@gmail.com>
2022-01-19 20:04:06 +0000

    Setup test framework to work with ES6 scripts

--- test/test.js
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-
-describe("Test", function() {
-    it("should run", function(){
-        expect(1).to.equal(1);
-    });
-});
---
+++ test/test_utils.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import constructLink from "../js/utils";
+
+describe("Utils", function() {
+    describe("#constructLink()", function() {
+        it("should pass this default test.", function() {
+            expect(1).to.equal(1);
+        });
+    });
+});
