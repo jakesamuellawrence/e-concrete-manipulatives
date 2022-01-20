07847ed9dff5a8f9bace6ed0b1b102f85bae4abf
Ada Fuge <2440986F@student.gla.ac.uk>
2022-01-20 00:08:43 +0000

    Visual Overhaul
    Changed the ground to a solid green texture and a disc shape rather than a rectangle
    Removed unnused textures
    Tweaked the sky colour
    Tweaked lighting
    Started to add background sprites
    Added UI for adding sticks and changing bundle size (Closes #49)
    Made sticks skinnier (Closes #50)

--- main.js
+++ main.js
@@ -4,12 +4,15 @@ import {StickSpawner} from './js/StickSpawner';
 import {constructLink, changeDimension, setup} from './js/utils';
 import { Plane, PlaneBufferGeometry, Vector3 } from 'three';
 import { RelativeDragControls } from './js/RelativeDragControls';
+import * as converter from 'number-to-words';
 
 //setup
 var baseURL = window.location.origin;
+let sticksInABundle = 5;
+document.getElementById("sticksInABundle").innerText = converter.toWords(sticksInABundle);
 
 //setting up colour
-let objectColour = "#8f5d46";
+let objectColour = "#8C5D41";
 
 function colourUpdate(event){
   objectColour = event.target.value;
@@ -38,7 +41,7 @@ const renderer = new THREE.WebGLRenderer({
 setup(scene, camera, renderer);
 
 
-const stickSpawner = new StickSpawner(scene, new Vector3(-1, 0.2, 0));
+const stickSpawner = new StickSpawner(scene, new Vector3(-0.5, 0.2, 0.7));
 
 const draggableList = [];
 const movementPlane = new Plane(new Vector3(0, 1, 0), -stickSpawner.stickParameters.radius);
@@ -49,6 +52,8 @@ controls.onHover = function(object) {
 controls.onUnhover = function(object) {
   object.material.emissive.set(0x000000);
 }
+
+
 
 stickSpawner.stickParameters.color = objectColour;
 spawnStick();
@@ -61,7 +66,10 @@ function animate(){
 
 // Make the buttons do their jobs
 document.getElementById("getLink").addEventListener('click', function(){constructLink(objectColour);})
-document.getElementById("newStick").onclick = spawnStick;
+document.getElementById("addStick").onclick = spawnStick;
+document.getElementById("add10Sticks").onclick = spawn10Sticks;
+document.getElementById("moreSticksInABundle").onclick = moreSticksInABundle;
+document.getElementById("fewerSticksInABundle").onclick = fewerSticksInABundle;
 document.getElementById("addxDimension").onclick = function(){changeDimension("x", 1, camera, console);}
 document.getElementById("minusxDimension").onclick = function(){changeDimension("x", -1, camera, console);}
 document.getElementById("addyDimension").onclick = function(){changeDimension("y", 1, camera, console);}
@@ -69,13 +77,40 @@ document.getElementById("minusyDimension").onclick = function(){changeDimension(
 document.getElementById("addzDimension").onclick = function(){changeDimension("z", 1, camera, console);}
 document.getElementById("minuszDimension").onclick = function(){changeDimension("z", -1, camera, console);}
 
+function moreSticksInABundle() {
+  if (sticksInABundle < 12){
+    sticksInABundle += 1
+  }
+  document.getElementById("sticksInABundle").innerText = converter.toWords(sticksInABundle);
+}
+
+function fewerSticksInABundle() {
+  if (sticksInABundle > 2){
+    sticksInABundle -= 1
+  }
+  document.getElementById("sticksInABundle").innerText = converter.toWords(sticksInABundle);
+}
 
 function spawnStick() {
   stickSpawner.position.setX(stickSpawner.position.x + 0.2);
   const stick = stickSpawner.spawn();
   draggableList.push(stick);
+
+  if(draggableList.length == 1){
+    document.getElementById("areInTotal").innerText = "There is";
+    document.getElementById("sticksInTotal").innerText = "stick in total";
+  }else{
+    document.getElementById("areInTotal").innerText = "There are";
+    document.getElementById("sticksInTotal").innerText = "sticks in total";
+  }
+  document.getElementById("numberInTotal").innerText = converter.toWords(draggableList.length);
 }
 
+function spawn10Sticks() {
+  for (let i=0;i<10;i++){
+    spawnStick();
+  }
+}
 
 
 animate();
